9030ca1ac511e1e5784b481f5bd9d70837117b79
Joey Hilton <joey@masteryconnect.com>
2015-10-14 12:15:13 -0600

    added to scrolls sections test

--- overview/overview_spec.rb
+++ overview/overview_spec.rb
@@ -13,11 +13,11 @@ describe "OverviewSpec" do
 
   before(:each) do
     @driver = Selenium::WebDriver.for :firefox
-    @base_url = "https://www.masteryconnect.com/"
+    @base_url = "https://www-staging.masteryconnect.com/"
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
     @verification_errors = []
-    @url_path = "/overview/"
+    @url_path = "/overview/#"
     @driver.get(@base_url + @url_path)
   end
   
@@ -26,20 +26,37 @@ describe "OverviewSpec" do
     @verification_errors = []
   end
   
-  it "opens_closes_form_modal" do
-    @driver.find_element(:link, "request a demo").click
-    @driver.find_element(:css, "div.modal_close").click
-  end
+  # it "opens_closes_form_modal" do
+  #   @driver.find_element(:link, "request a demo").click
+  #   @driver.find_element(:css, "div.modal_close").click
+  # end
 
   it "scrolls_sections_plays_closes_videos" do 
+    @driver.find_element(:xpath, "//*[@id='cta']/a").click
+    wait_for(10) { @driver.find_element(:xpath, "//*[@id='last']/div/div/h2").displayed? }
+    (@driver.find_element(:xpath, "//*[@id='last']/div/div/h2").text).should == "Fuel Student Growth"
+    @driver.find_element(:xpath, "//li[1]/span").click
+    wait_for(10) { @driver.find_element(:xpath, "//*[@id='home']/div/div/h1").displayed? }
+    (@driver.find_element(:xpath, "//*[@id='home']/div/div/h1").text).should == "Student learning identified."
+
     @driver.find_element(:xpath, "//li[2]/span").click
     @driver.find_element(:class, "video1_play").click
     @driver.find_element(:css, "#video1 > div.modal_close").click
+    @driver.find_element(:xpath, "//*[@id='question1']/div/div/div[1]/a/i").click
+    @driver.find_element(:css, "#video1 > div.modal_close").click
+
+    @driver.find_element(:xpath, "//*[@id='cta']/a").click
     @driver.find_element(:xpath, "//li[3]/span").click
     @driver.find_element(:css, "a.video2_play.video_play > i.icon-playback-play").click
     @driver.find_element(:css, "#video2 > div.modal_close").click
+    @driver.find_element(:xpath, "//*[@id='question2']/div/div/div[1]/a/i").click
+    @driver.find_element(:css, "#video2 > div.modal_close").click
+
+    @driver.find_element(:xpath, "//*[@id='cta']/a").click
     @driver.find_element(:xpath, "//li[4]/span").click
     @driver.find_element(:css, "a.btn-primary.video3_play").click
+    @driver.find_element(:css, "#video3 > div.modal_close").click
+    @driver.find_element(:xpath, "//*[@id='question3']/div/div/div[1]/a/i").click
     @driver.find_element(:css, "#video3 > div.modal_close").click
   end
 
@@ -47,9 +64,9 @@ describe "OverviewSpec" do
       @driver.find_element(:xpath, "//li[5]/span").click
     # @driver.find_element(:link, "Request a Demo").click
     # @driver.find_element(:css, "#form_success > div.modal_close").click
-    @driver.find_element(:class, "request_demo").click
+    # @driver.find_element(:class, "request_demo").click
     @driver.find_element(:id, "full_name").clear
-    @driver.find_element(:id, "full_name").send_keys "mac firefox"
+    @driver.find_element(:id, "full_name").send_keys "test firefox"
     Selenium::WebDriver::Support::Select.new(@driver.find_element(:id, "role")).select_by(:text, "School Administrator")
     @driver.find_element(:id, "email").clear
     @driver.find_element(:id, "email").send_keys "firefox@test.com"
@@ -61,6 +78,24 @@ describe "OverviewSpec" do
     # ERROR: Caught exception [ERROR: Unsupported command [fireEvent | id=zip_code | blur]]
     Selenium::WebDriver::Support::Select.new(@driver.find_element(:id, "school_list")).select_by(:text, "WEST BOUNTIFUL SCHOOL")
     @driver.find_element(:id, "submit").click
+
+    verify { element_present?(:id, "form_success").should be_true }
+    (@driver.find_element(:xpath, "//*[@id='form_success']/h2").text).should == "Thanks!"
+    verify { element_present?(:xpath, "//*[@id='form_success']/p[2]/a").should be_true }
+
+    first_window = @driver.window_handle
+    @driver.find_element(:xpath, "//*[@id='form_success']/p[2]/a").click
+    all_windows = @driver.window_handles
+    new_window = all_windows.select { |this_window| this_window != first_window }
+
+    @driver.switch_to.window(first_window)
+
+    @driver.switch_to.window(new_window)
+    expect(@driver.title).to eql 'MasteryConnect'
+    verify { (@driver.current_url).should == "https://www.masteryconnect.com/?utm_source=web_app_masteryconnect&utm_medium=link&utm_campaign=overview" }
+    @driver.close
+    @driver.switch_to.window(first_window)
+
     verify { element_present?(:link, "OK, GOT IT").should be_true }
     @driver.find_element(:link, "OK, GOT IT").click
   end
@@ -69,7 +104,7 @@ describe "OverviewSpec" do
       @driver.find_element(:link, "masteryconnect.com").click
     # ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp |  | ]]
     # ERROR: Caught exception [ERROR: Unsupported command [selectPopUp |  | ]]
-    verify { (@driver.current_url).should == "https://www.masteryconnect.com/" }
+    verify { (@driver.current_url).should == "https://www.masteryconnect.com/?utm_source=web_app_masteryconnect&utm_medium=link&utm_campaign=overview" }
     @driver.close
     # ERROR: Caught exception [ERROR: Unsupported command [selectWindow |  | ]]
   end
@@ -79,6 +114,10 @@ describe "OverviewSpec" do
     true
   rescue Selenium::WebDriver::Error::NoSuchElementError
     false
+  end
+
+  def wait_for(seconds)
+    Selenium::WebDriver::Wait.new(timeout: seconds).until { yield }
   end
   
   def alert_present?()
