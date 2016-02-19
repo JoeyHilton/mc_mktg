d0cd643245bb660b1622187a3beba7110f1d3fd1
Joey Hilton <joey@masteryconnect.com>
2016-02-19 16:18:13 -0700

    fixed webinar spec hook

--- contact forms/spec/webinar_spec.rb
+++ contact forms/spec/webinar_spec.rb
@@ -14,8 +14,8 @@ describe "request demo form" do
     @form.click(:xpath, "//*[@id='webinar_premium']/div[1]/a/span")
     @form.info_form_present?.should be true
     @form.with('Webinar',  'Test', 'Chief', '208-456-1237', Faker::Internet.email)
-    @form.select_dropdown(:xpath, "//*[@id='form_area_one']/fieldset[2]/label[1]/span/select", "Teacher")
-    @form.select_dropdown(:xpath, "//*[@id='form_area_one']/fieldset[2]/label[2]/span/select", "ID")
+    @form.select_dropdown(:xpath, "//*[@id='form_area_one']/fieldset[2]/label[1]/select", "Teacher")
+    @form.select_dropdown(:xpath, "//*[@id='form_area_one']/fieldset[2]/label[2]/select", "ID")
     @form.click(:xpath, "//*[@id='radio-btns']/div[1]/span/input")
     @form.click(:class, "lte_gry_rnd_btn")
     @form.success_message_present?.should be true
